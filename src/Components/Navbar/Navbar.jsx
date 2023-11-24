813e999d912cf7abce267e9f7b001443da367251
Lalit Jakhar <lalitjakhar@Rajeshs-iMac.local>
2023-11-24 10:35:17 +0530

    initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,11 +5,9 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import BroadcastIcon from "@mui/icons-material/BroadcastOnPersonalRounded";
 import { useNavigate } from "react-router-dom";
 import Profile from "../Profile/Profile";
@@ -19,28 +17,14 @@ const pages = ["Products", "Pricing", "Contact", "Location"];
 function Navbar() {
   const navigate = useNavigate();
 
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
-  };
-
-  const handleProfileClick = () => {
-    handleCloseUserMenu();
-    navigate("/Profile");
   };
 
   return (
